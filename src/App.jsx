a1a8f842a58bbeb0b5ac0c8447dd4e346b5b4c21
Beaver0001 <mkhalifaev@gmail.com>
2025-06-29 01:57:46 +0500

    🔥 агрузка полной версии PlovCoin

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-yellow-100 text-center p-10">
+      <h1 className="text-4xl font-bold">PlovCoin is Cooking 🍲</h1>
+    </div>
+  )
+}
+
+export default App
